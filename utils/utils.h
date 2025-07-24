0f1969bbed3bd856e23afb56423a45cc8b7eab0d
qeka <qekaqek@gmail.com>
2025-07-24 14:59:21 +0300

    feat: Add min max macros

--- utils/utils.h
+++ utils/utils.h
@@ -8,3 +8,5 @@
 #define shiftptr(ptr, off) ((typeof(ptr))((char *)(ptr) + (off)))
 #define countof(X) (sizeof(X) / sizeof((X)[0]))
 #define containerof(type, ptr, memb) ((type *)(shiftptr((ptr), -offsetof(type, memb))))
+#define min(X, Y) ((X) > (Y) ? (Y) : (X))
+#define max(X, Y) ((X) > (Y) ? (X) : (Y))
